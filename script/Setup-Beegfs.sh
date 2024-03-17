1ce875fbf08b074f63950c37d35b5f9c54b40b43
Cloud User <ec2-user@ip-10-0-6-72.eu-west-2.compute.internal>
2024-03-17 04:05:46 +0000

    fail

--- script/Setup-Beegfs.sh
+++ script/Setup-Beegfs.sh
@@ -1,6 +1,4 @@
 sudo dd if=/dev/random of=../gadget/connauthfile bs=128 count=1
-sudo chmod 400 ../gadget/connauthfile
-sudo cp ../gadget/connauthfile /etc/beegfs/connauthfile
 cd ../ansible
 ansible-playbook --private-key ~/.ssh/host_key -i inventory.yaml beegfs-packages.yaml
 ansible-playbook --private-key ~/.ssh/host_key -i inventory.yaml beegfs-host.yaml
