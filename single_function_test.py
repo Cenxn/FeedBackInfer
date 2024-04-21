8b4352bfa53d8e4687094b507936d61117f6cacb
Cenxn <1017435995@qq.com>
2024-04-22 00:14:37 +0100

    debug

--- single_function_test.py
+++ single_function_test.py
@@ -1,4 +1,5 @@
-from celery_code.src.celery_app import prepare_inference_tasks, inference_single_csv, distribute_csv_file_no_generate, prepare_inference_tasks
+from celery_code.src.celery_app import app, prepare_inference_tasks, inference_single_csv, \
+    distribute_csv_file_no_generate, prepare_inference_tasks
 from celery import group, chain, chord
 
 df_path = r'/beegfs-FeedBackInfer/input/feedback-prize-effectiveness/test.csv'
@@ -10,7 +11,13 @@ callback_chain = chain(distribute_task, prepare_inference_tasks.s()).apply_async
 
 task_signatures = callback_chain.get()
 print(f'\nHIGHLIGHT!!!!!\n {task_signatures} type: \n {type(task_signatures)}')
-result_group = group(task_signatures)
+
+task_list = [
+    app.signature(task['task'], args=task['args'], kwargs=task['kwargs'],
+                  options=task['options'], immutable=task['immutable']) for task in task_signatures
+]
+
+result_group = group(task_list)
 result = result_group.apply_async()
 
 print(result.get())
