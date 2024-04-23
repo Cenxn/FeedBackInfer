d513e9d6dfdb59f6ab955dc2941d603c6d5048d1
Cenxn <1017435995@qq.com>
2024-04-23 18:00:00 +0100

    debug

--- feedback_infer_dj/feedback_infer_dj/urls.py
+++ feedback_infer_dj/feedback_infer_dj/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..upload_essay import views
+from upload_essay import views
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
