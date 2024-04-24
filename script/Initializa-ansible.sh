ddec43720a85e13f97b4b671889aec523651b31e
Mingzirui WU <100745292+Cenxn@users.noreply.github.com>
2024-04-24 14:17:34 +0100

    Update Initializa-ansible.sh

--- script/Initializa-ansible.sh
+++ script/Initializa-ansible.sh
@@ -1,8 +1,5 @@
-git clone https://github.com/Cenxn/FeedBackInfer.git
-cd FeedBackInfer/script/
-./Initialize-ansible.sh
-./Setup-Beegfs.sh
-./Setup-python-env.sh
-./Setup_Celery-with-Django.yaml
-# or 
-# Setup-Celery-only.yaml
+sudo dnf install python3-pip
+pip install ansible
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/host_key -N ""
+cd ../ansible
+ansible-playbook --private-key ~/.ssh/comp0239_key -i inventory.yaml sshkeys-host.yaml
